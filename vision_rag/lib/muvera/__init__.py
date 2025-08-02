56ff9a06c1feaa3f0116230e029aaa01a23dd290
YeonwooSung <neos960518@gmail.com>
2025-08-03 00:23:52 +0900

    feat: Implement the muvera algorithm

---
+++ vision_rag/lib/muvera/__init__.py
@@ -0,0 +1,12 @@
+from .fde_generator import (
+    FixedDimensionalEncodingConfig,
+    generate_query_fde,
+    generate_document_fde_batch,
+)
+
+
+__all__ = [
+    "FixedDimensionalEncodingConfig",
+    "generate_query_fde",
+    "generate_document_fde_batch",
+]
